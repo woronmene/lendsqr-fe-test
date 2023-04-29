8c7659856912c1bb21244fae531f7ac1d66093b0
Woron Mene <meneworon4@gmail.com>
2023-04-29 16:35:07 +0100

    Implement mobile view of layout

--- src/routes/Root/Root.tsx
+++ src/routes/Root/Root.tsx
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../../components/Navbar/Navbar";
+import styles from "./Root.module.scss";
+import Sidebar from "../../components/Sidebar/Sidebar";
+import { Outlet } from "react-router-dom";
+// import { getAllUsers, getUserById, User } from "../../utils/users";
 
 // type RootProps = {
 
 // };
 
 const Root: React.FC = () => {
+  const [showNav, setShowNav] = useState<boolean>(true);
+  const toggleNav = () => {
+    setShowNav(!showNav);
+  };
+
   return (
-    <div>
-      <Navbar />
+    <div className={styles.root}>
+      <Navbar toggle={toggleNav} />
+      <div className={styles.sidebarLayout}>
+        <Sidebar showNav={showNav} setShowNav={setShowNav} />
+
+        <Outlet />
+      </div>
     </div>
   );
 };
