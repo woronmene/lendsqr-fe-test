d3cd08f284706631b3c5563de3c9885c16d624cb
Woron Mene <meneworon4@gmail.com>
2023-05-02 12:36:49 +0100

    remove test files

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: "happy-dom",
-  },
 });
